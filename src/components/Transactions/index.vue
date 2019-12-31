ee37395e0cdc515c0b08f731eb4cb4c59c62c65a
Quentin Vacher <qvacher@gmail.com>
2019-12-31 20:02:35 +0100

    feat(card-details) : Transactions UI done

--- src/components/Transactions/index.vue
+++ src/components/Transactions/index.vue
@@ -1,33 +1,38 @@
 <template>
-    <v-flex xs12 class='pa-3'>
-        <v-card>
-          <v-card-title class="pb-0">
-            <h3 v-if='amount' class="grey--text card__amount">{{ amount }} €</h3>
-            <h3 v-else class="grey--text card__amount"><pre>-</pre></h3>
-            <h2 class='font-weight-light card__title'><pre>{{ title }}</pre></h2>
-          </v-card-title>
-          <v-card-text>
-            <pre class="card__code">{{ code }}</pre>
-            <pre class="card__owner">{{ owner }}</pre>
-          </v-card-text>
-        </v-card>
-    </v-flex>
+  <v-flex xs12 class="pa-2">
+    <v-card class="transaction">
+      <v-card-text>
+        <v-avatar size="40px" color="#D64158" class="mr-4">
+          <span class="grey--text headline">Q</span>
+        </v-avatar>
+        <h2 class="grey--text transaction__title">{{ title }}</h2>
+        <v-spacer />
+        <h3 class="transaction__amount">
+          <span v-if="type === 'credit'" class="transaction__amount-credit">+</span>
+          <span v-else class="transaction__amount-debit">-</span>
+          <span class="grey--text font-weight-light">{{ amount }}</span>
+          <span class="grey--text-darken transaction__amount-devise">€</span>
+        </h3>
+        <v-icon v-if="type === 'credit'" color="success" class="ml-2">call_made</v-icon>
+        <v-icon v-else color="#D64158" class="ml-2">call_received</v-icon>
+      </v-card-text>
+    </v-card>
+  </v-flex>
 </template>
 
 <script>
 export default {
-  name: 'Cards',
+  name: "Cards",
   components: {},
   props: {
-    title: {type: String, default: null},
-    amount: {type: String, default: null},
-    type: {type: String, default: null},
-    code: {type: String, default: null}
+    title: { type: String, default: null },
+    amount: { type: String, default: null },
+    type: { type: String, default: null },
+    code: { type: String, default: null }
   },
-  data () {
-    return {
-    }
+  data() {
+    return {};
   },
   methods: {}
-}
+};
 </script>
--- src/views/cards.vue
+++ src/views/cards.vue
@@ -6,26 +6,22 @@
       </v-layout>
       <v-layout row wrap>
         <Card
-            v-for='card in cards'
-            :key='card.code'
-            :title='card.title'
-            :amount='card.amount'
-            :owner='card.owner'
-            :code='card.code'
-            @delete-card="deleteCard($event)"
-            @details-card="detailsCard($event)"
+          v-for="card in cards"
+          :key="card.code"
+          :title="card.title"
+          :amount="card.amount"
+          :owner="card.owner"
+          :code="card.code"
+          @delete-card="deleteCard($event)"
+          @details-card="detailsCard($event)"
         />
-        <v-flex xs12 sm6 md6 lg3 xl3 class='pa-3'>
-            <v-btn class='card__add' @click.stop="dialog = true">
-                <v-icon class="mr-2">add</v-icon>
-                <span>Add a new card</span>
-            </v-btn>
+        <v-flex xs12 sm6 md6 lg3 xl3 class="pa-3">
+          <v-btn class="card__add" @click.stop="dialog = true">
+            <v-icon class="mr-2">add</v-icon>
+            <span>Add a new card</span>
+          </v-btn>
         </v-flex>
-        <Modal
-          :open="dialog"
-          @add-card="addCard($event)"
-          @close="dialog = false;"
-        />
+        <Modal :open="dialog" @add-card="addCard($event)" @close="dialog = false;" />
       </v-layout>
     </v-layout>
     <v-layout column v-if="detailedView" key="detail">
@@ -34,21 +30,23 @@
       </v-layout>
       <v-layout row class="justify--content-center">
         <CardDetail
-          :key='selectedCard.code'
-          :title='selectedCard.title'
-          :amount='selectedCard.amount'
-          :owner='selectedCard.owner'
-          :code='selectedCard.code'
+          :key="selectedCard.code"
+          :title="selectedCard.title"
+          :amount="selectedCard.amount"
+          :owner="selectedCard.owner"
+          :code="selectedCard.code"
         />
+      </v-layout>
+      <v-layout row class="justify--content-center">
+        <p>Pagination</p>
       </v-layout>
       <v-layout row wrap class="justify--content-center">
         <Transaction
-          v-for='transaction in transactions'
-          :key='transaction.code'
-          :title='transaction.title'
-          :amount='transaction.amount'
-          :code='transaction.code'
-          :type='transaction.type'
+          v-for="transaction in transactions"
+          :key="transaction.code"
+          :title="transaction.title"
+          :amount="transaction.amount"
+          :type="transaction.type"
         />
       </v-layout>
     </v-layout>
@@ -56,55 +54,117 @@
 </template>
 
 <script>
-import Card from '@/components/Cards/List'
-import CardDetail from '@/components/Cards/Detail'
-import Modal from '@/components/Modals'
-import Transaction from '@/components/Transactions'
+import Card from "@/components/Cards/List";
+import CardDetail from "@/components/Cards/Detail";
+import Modal from "@/components/Modals";
+import Transaction from "@/components/Transactions";
 
 export default {
-  name: 'Cards',
+  name: "Cards",
   components: {
     Card,
     CardDetail,
     Modal,
     Transaction
   },
-  props: {
-
-  },
-  data () {
+  props: {},
+  data() {
     return {
       cards: [
-        {title: 'Auchan', amount: '4,3', owner: 'Quentin Vacher', code: '0491 1275 2176 0197'},
-        {title: 'Casino', amount: '8,7', owner: 'Quentin Vacher', code: '0491 1281 2172 0193'},
-        {title: 'E-Leclerc', amount: '0,67', owner: 'Quentin Vacher', code: '0441 1294 2175 0196'},
-        {title: 'Feu Vert', amount: '', owner: 'Quentin Vacher', code: '0491 1294 2175 0196'},
-        {title: 'UGC', amount: '', owner: 'Quentin Vacher', code: '0491 1307 2178 0199'},
-        {title: 'Gaumont Pathé', amount: '', owner: 'Quentin Vacher', code: '0491 1307 2174 0199'},
-        {title: 'Midas', amount: '', owner: 'Quentin Vacher', code: '0492 1307 2178 0199'}
+        {
+          title: "Auchan",
+          amount: "4,3",
+          owner: "Quentin Vacher",
+          code: "0491 1275 2176 0197"
+        },
+        {
+          title: "Casino",
+          amount: "8,7",
+          owner: "Quentin Vacher",
+          code: "0491 1281 2172 0193"
+        },
+        {
+          title: "E-Leclerc",
+          amount: "0,67",
+          owner: "Quentin Vacher",
+          code: "0441 1294 2175 0196"
+        },
+        {
+          title: "Feu Vert",
+          amount: "",
+          owner: "Quentin Vacher",
+          code: "0491 1294 2175 0196"
+        },
+        {
+          title: "UGC",
+          amount: "",
+          owner: "Quentin Vacher",
+          code: "0491 1307 2178 0199"
+        },
+        {
+          title: "Gaumont Pathé",
+          amount: "",
+          owner: "Quentin Vacher",
+          code: "0491 1307 2174 0199"
+        },
+        {
+          title: "Midas",
+          amount: "",
+          owner: "Quentin Vacher",
+          code: "0492 1307 2178 0199"
+        }
       ],
       dialog: false,
       detailedView: false,
       selectedCard: {},
       transactions: [
-        {title: 'Paiement', amount: '4', type: 'debit', code: '0491 1275 2176 0197'},
-        {title: 'Credit from gift card', amount: '4,15', type: 'credit', code: '0491 1275 2176 0197'},
-        {title: 'Credit from coupon', amount: '4', type: 'credit', code: '0491 1275 2176 0197'},
-        {title: 'Credit from promo', amount: '0,15', type: 'credit', code: '0491 1275 2176 0197'}
+        {
+          title: "Paiement",
+          amount: "4",
+          type: "debit",
+          code: "0491 1275 2176 0197"
+        },
+        {
+          title: "Credit from gift card",
+          amount: "4,15",
+          type: "credit",
+          code: "0491 1275 2176 0197"
+        },
+        {
+          title: "Credit from coupon",
+          amount: "4",
+          type: "credit",
+          code: "0491 1275 2176 0197"
+        },
+        {
+          title: "Credit from promo",
+          amount: "0,15",
+          type: "credit",
+          code: "0491 1275 2176 0197"
+        }
       ]
-    }
+    };
   },
   methods: {
-    addCard (card) {
-      this.cards.push(card)
+    addCard(card) {
+      this.cards.push(card);
     },
-    deleteCard (code) {
-      this.cards.splice(this.cards.indexOf(this.cards.find(function (card) { return card.code === code })), 1)
+    deleteCard(code) {
+      this.cards.splice(
+        this.cards.indexOf(
+          this.cards.find(function(card) {
+            return card.code === code;
+          })
+        ),
+        1
+      );
     },
-    detailsCard (code) {
-      this.detailedView = !this.detailedView
-      this.selectedCard = this.cards.find(function (card) { return card.code === code })
+    detailsCard(code) {
+      this.detailedView = !this.detailedView;
+      this.selectedCard = this.cards.find(function(card) {
+        return card.code === code;
+      });
     }
   }
-}
+};
 </script>
